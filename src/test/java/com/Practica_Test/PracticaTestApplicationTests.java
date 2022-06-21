d64b7605b5867d443e8eeaa60add29ee5cd6f8cb
GhostFix7 <brandruiz7@gmail.com>
2022-06-20 18:51:20 -0600

    Prueba1

---
+++ src/test/java/com/Practica_Test/PracticaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
